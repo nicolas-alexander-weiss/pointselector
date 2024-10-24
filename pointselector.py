8156d916a8d98c2b084d71f012a00206ace78eef
Nicolas Alexander Weiss <nicoalex2912@gmail.com>
2024-10-24 11:27:48 +0200

    Basic running product.

---
+++ pointselector.py
@@ -0,0 +1,94 @@
+"""
+@author: nicolas-alexander-weiss
+@created: 2024-10-24
+
+@dependencies: numpy, matplotlib
+
+"""
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+class Point_2D:
+    """Simple wrapper for a 2D point."""
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __repr__(self) -> str:
+        return f"{type(self).__name__}(x={self.x}, y={self.y})"
+
+    def to_numpy(self):
+        return np.array([self.x, self.y])
+
+
+class Point_Selector:
+
+    def __init__(self, x_range=(0, 1), y_range=(0, 1)):
+
+        self.selected_points = set()
+
+        ##### PYPLOT CODE ##########
+
+        # Create a figure and axis
+        self.fig, self.ax = plt.subplots()
+
+        # Set limits, aspect ratio and grid
+        self.ax.set_xlim(x_range)
+        self.ax.set_ylim(y_range)
+        self.ax.set_aspect("equal", adjustable="box")
+        self.ax.grid(True)
+
+        # Connect the click event handler
+        self.fig.canvas.mpl_connect("button_press_event", self)
+        ###############################
+
+    def __call__(self, event):
+        x, y = event.xdata, event.ydata
+        if x is not None and y is not None:
+            # add the point to selection
+            self.selected_points.add(Point_2D(x, y))
+            # Plot the point
+            self.ax.plot(x, y, "ro")  # Red dot for clicked points
+            self.fig.canvas.draw()  # Redraw the figure
+
+    def run(self):
+        plt.show()
+
+    def get_points(self):
+        """Getter function for the selected points.
+
+        return
+        ----------
+        set of Point_2D
+        """
+        return self.selected_points
+
+    def get_points_numpy(self):
+        """Returns the set of points as a numpy array.
+
+        return
+        ----------
+        2d numpy array, each row a point.
+        """
+        return np.array([p.to_numpy() for p in self.selected_points])
+
+
+if __name__ == "__main__":
+
+    print(
+        "-- Select points by clicking into the pyplot figure and then closing the window. --"
+    )
+
+    x_range = (0, 1)
+    y_range = (0, 1)
+
+    point_selector = Point_Selector(x_range, y_range)
+    point_selector.run()
+
+    points = point_selector.get_points_numpy()
+
+    # Outputs selected points:
+    print(f"Selected points:\n{points}")
